7ba52ba825100126bfca3e748a7c2cc58deb6179
Ivan Mauricio Diaz <idiaz@conexia.com>
2022-11-19 08:07:17 -0500

    Fix: path google sheet prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  google_sheet_api: 'https://script.google.com/macros/s/AKfycbwt-36ppCqbPcCXMIIwEpBQ36HxVRUpSU-dpghM34_-8PxEs3RmCnyvAaWrT2xmHtry'
 };
